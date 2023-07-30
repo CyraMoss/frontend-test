8ef3d57fee6caf52e11989e08de73b5f34c648fa
Cyra Moss <114317802+CyraMoss@users.noreply.github.com>
2023-07-31 06:15:02 +1200

    styled product details a bit

--- frontendtest/src/pages/index.tsx
+++ frontendtest/src/pages/index.tsx
@@ -36,35 +36,41 @@ export default function ProductPage() {
         <meta name="description" content="Made for a technical test" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
+      <main className="flex min-h-screen pt-6">
         {product ? (
-          <div>
-          <div className="header">Product Details</div>
-          <div className="product">
+          <div className="flex-1 flex">
             <Image
-              src={product.imageURL} // Use "imageURL" instead of "image_url"
+              src={product.imageURL}
               alt="Product Image"
-              width={150}
-              height={150}
+              layout="responsive" // Set the layout to responsive
+              width={300} // Set a fixed width for the image
+              height={400} // Set a fixed height for the image
+              objectFit="contain" // Adjust the image to maintain its aspect ratio
             />
-            <div className="product-details">
-              <h2>{product.title}</h2> {/* Use "title" instead of "name" */}
-              <p>{product.description}</p>
-              <p>Price: ${product.price.toFixed(2)}</p>
-              <div className="size-options">
-                {/* <!-- Size selection options will be populated dynamically using JavaScript --> */}
-              </div>
-              <button className="add-to-cart-btn">Add to Cart</button>
-              <div className="error-message"></div>
-            </div>
-          </div>
           </div>
         ) : (
           <div>Loading...</div>
         )}
-        <div className="mini-cart">
-          {/* <!-- Mini cart content will be populated dynamically using JavaScript --> */}
-        </div>
+        {product && (
+          <div className="flex-1 flex flex-col pl-4">
+            <div className="product-details">
+              <h2 className="text-2xl font-semibold py-4">{product.title}</h2>
+              <hr/>
+              <p className="font-bold py-2"> ${product.price.toFixed(2)}</p>
+              <p>{product.description}</p>
+              <div className="size-options">
+                <h3 className="font-bold py-2">Sizes:</h3>
+                <ul className="flex mb-4">
+                  {product.sizeOptions.map((size) => (
+                    <li key={size.id} className="p-4 m-2 border-2 border-black">{size.label}</li>
+                  ))}
+                </ul>
+              </div>
+              <button className="border-2 border-black px-5 py-2">Add to Cart</button>
+              <div className="error-message"></div>
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
